41a881e12df86b6ba1a19aeb1d2d4a11312b80eb
Giedre <saulevakarine1@gmail.com>
2019-04-23 15:45:49 +0300

    katasuniai

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
-$x = rand(1, 10);
 
-for ($z = 0; $z < $x; $z++) {
-    print "Tai yra $z-tasis ciklas<br>";
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+
+$katasuniai = 0;
+
+for ($k = 1; $k <= $kates; $k++) {
+    for ($s = 1; $s <= $sunys; $s++) {
+        $success = rand(0, 1);
+        
+        if ($success) {
+            $katasuniai++;
+            break;
+        }
+    }
 }
+
+print "Event`e dalyvavo $kates kates ir $sunys sunys.<br>"
+        . "Katasuniu iseiga: $katasuniai";
 ?>
 <!DOCTYPE html>
 <html>
