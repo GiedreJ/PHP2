fa41a2c0840d22e4079ef3bfb693688488ac904f
Giedre <saulevakarine1@gmail.com>
2019-04-23 16:50:43 +0300

    balansas

--- index.php
+++ index.php
@@ -6,21 +6,18 @@ $alga = 700;
 
 
 for ($months = 1; $months <= 12; $months++) {
-   $islaidos = rand(500,900);
+   $islaidos = rand(500, 900);
    $balansas = $kisene + $alga - $islaidos;
    print "balansas $balansas<br>";
    print "islaidos $islaidos<br>";
-   if ($balansas <=0) {
-       print '"Bloga prognoze: $months gali baiktis pinigai! Atsargiai!<br>"';
+   if ($balansas <=500) {
+       print "Bloga prognoze: $months gali baigtis pinigai! Atsargiai!<br>";
        break;
    } else {
        $kisene = $balansas;
    }
-
-
 }
 
-$text = "likutis $kisene";
 ?>
 <!DOCTYPE html>
 <html>
