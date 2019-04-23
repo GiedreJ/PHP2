616f8bcda46763f553d261350f64f30993ccef27
Giedre <saulevakarine1@gmail.com>
2019-04-23 17:32:39 +0300

    cizos

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
 
-$months = 12;
-$kisene = 1000;
-$alga = 700;
+//$days = 365;
+$cizos_mon_thu = date("N");
+$cizos_fri = rand(10, 20);
+$cizos_sat_sun = rand(1, 5);
+$pakelio_kaina = 3.40;
+$surukyta = 0;
 
+for ($day = 1; $day < 365; $day++){
+  $weekday = date('N', strtotime("+$day days"));
 
-for ($months = 1; $months <= 12; $months++) {
-   $islaidos = rand(500, 900);
-   $balansas = $kisene + $alga - $islaidos;
-   print "balansas $balansas<br>";
-   print "islaidos $islaidos<br>";
-   if ($balansas <=500) {
-       print "Bloga prognoze: $months gali baigtis pinigai! Atsargiai!<br>";
-       break;
-   } else {
-       $kisene = $balansas;
-   }
+  if ($weekday <= 4){
+      $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
+      $surukyta += $cizos_mon_thu; // Kiek sur?kiau iki ketvirtadienio
+      print "$surukyta<br>";
+  } elseif ($weekday == 5){
+      $cizos_fri = rand(10, 20);
+      $surukyta += $cizos_fri;
+  } else {
+      $cizos_sat_sun = rand(1, 3);
+      $surukyta += $cizos_sat_sun;
+  }
 }
+
+$text = "Visos surukytos cizos (vnt) $surukyta";
 
 ?>
 <!DOCTYPE html>
