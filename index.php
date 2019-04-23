cad8a34faa269ddfd05275b73d6a5bdca6f26d10
Giedre <saulevakarine1@gmail.com>
2019-04-23 15:02:50 +0300

    for

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php print 'Veikia' ?>
+<?php
+$x = rand(1, 10);
+
+for ($z = 0; $z < $x; $z++) {
+    print "Tai yra $z-tasis ciklas<br>";
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        
+    </body>
+</html>
