9c95ec9fa15d264d406f042bf8cc96065a95d17e
Giedre <saulevakarine1@gmail.com>
2019-04-24 09:50:55 +0300

    cizos

--- index.php
+++ index.php
@@ -1,38 +1,36 @@
 <?php
-
-//$days = 365;
-$cizos_mon_thu = date("N");
-$cizos_fri = rand(10, 20);
-$cizos_sat_sun = rand(1, 5);
-$pakelio_kaina = 3.40;
+$pakelio_kaina = 3.75;
 $surukyta = 0;
+$surukyta2 = 0;
 
-for ($day = 1; $day < 365; $day++){
-  $weekday = date('N', strtotime("+$day days"));
-
-  if ($weekday <= 4){
-      $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
-      $surukyta += $cizos_mon_thu; // Kiek sur?kiau iki ketvirtadienio
-      print "$surukyta<br>";
-  } elseif ($weekday == 5){
-      $cizos_fri = rand(10, 20);
-      $surukyta += $cizos_fri;
-  } else {
-      $cizos_sat_sun = rand(1, 3);
-      $surukyta += $cizos_sat_sun;
-  }
+for ($day = 1; $day <= 365; $day++) {
+   $weekday = date('N', strtotime("+$day days"));
+   if ($weekday <= 4) {
+       $cizos_mon_thu = rand(3, 4); // Nuo pirmadienio iki ketvirtadienio
+       $surukyta += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
+       $surukyta2 += $cizos_mon_thu; // Kiek surūkiau iki ketvirtadienio
+   } elseif ($weekday == 5) {
+       $cizos_fri = rand(10, 20); // Penktadienis
+       $surukyta += $cizos_fri; // Kiek surukiau per penktadieni
+   } else {
+       $cizos_sat_sun = rand(1, 3); // Sestadienis-sek
+       $surukyta += $cizos_sat_sun; // Kiek surukiau per sest-sek
+   }
 }
-
-$text = "Visos surukytos cizos (vnt) $surukyta";
+$kainuos2 = round($surukyta2 / 20 * 3.75);
+$text2 = "Nerukydamas pirmadieni-ketvirtaideni sutaupyciau $kainuos2 eur.";
+$kainuos = round($surukyta / 20 * 3.75);
+$text = "Per metus surukysiu $surukyta cigareciu uz $kainuos eur.";
 
 ?>
-<!DOCTYPE html>
+
 <html>
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        
-    </body>
+   <head>
+       <title>main</title>
+   </head>
+   <body>
+       <P>
+           <?php print $text . ' ' . $text2;?>
+       </P>
+   </body>
 </html>
