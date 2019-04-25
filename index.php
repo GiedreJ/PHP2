cc5e41c35d4f5b820c22607eaebdeb45a227bc35
Giedre <saulevakarine1@gmail.com>
2019-04-25 11:00:33 +0300

    grikiai for'as su days

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
 <?php
-$grikiai = 5000;
-$grikiai_start = $grikiai;
-$per_diena = rand(200, 500);
-$praejo_dienu = "";
+    $grikiai_start = 5000;
+    $grikiai = $grikiai_start;
+    $survive_days = 0;
+    $result = "";
 
-for (; $grikiai >= 500 ;) {
-    $grikiai -= rand(200, 500);
-    $praejo_dienu++;
-    print "$praejo_dienu liks $grikiai" . '<br>';
-}
 
-$date = date('Y-M-d', strtotime("+$praejo_dienu days"));
-$text = "Jei turėsiu $grikiai_start g. grikių, valgysiu juos $praejo_dienu dienų."
-         . " Jei pradėsiu šiandien, grikiai baigsis $date data";
+    for($day = 1; $day < 100; $day++){
+        $sunaudojimas = rand(200,500);
+        if ($grikiai >= 500){
+
+            $grikiai -= $sunaudojimas;
+            $survive_days++;
+            $result .= "Grikiu yra $grikiai per $survive_days dienas." . '<br>';
+        }else{
+            $result .= "Paskutine diena $survive_days" . "grikiu liko $grikiai " . '<br>';
+            break;
+        }
+    }
+
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>ciklai</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    <body>
-        <p><?php print $text;?></p>
-    </body>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <?php
+            print $result;
+        ?>
+    </div>
+</body>
 </html>
