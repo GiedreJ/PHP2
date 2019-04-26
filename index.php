0905a4d71a3b9405317d8dd47c114dec19e308e6
Giedre <saulevakarine1@gmail.com>
2019-04-26 11:41:02 +0300

    function

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
 <?php
 
-function amzius($amzius) {
-if ($amzius < 0) {
-    return 'klaida';
-    }
-if ($amzius < 18) {
-    return 'nepilnametis';
-    }
-if ($amzius < 65) {
-    return 'pilnametis';
-    }    
-    return 'pensininkas';
-}
+$vardas = 'Petras Jonaitis';
+$amzius = 27;
+$statusas = 'dirba'; //studijuoja, dirba, nedirba
 
-print amzius();
+if ($amzius >= 0 && $amzius <18) {
+	echo $vardas . ' yra nepilanametis';
+	echo $vardas . ' netinkamas kariuomenei';
+
+} elseif ($amzius >= 18 && $amzius < 65) {
+	echo $vardas . ' yra pilnametis';
+	
+	if ($amzius <= 26) {
+		if ($statusas == 'dirba' || $statusas == 'nedirba') {
+			echo $vardas . ' Tinkamas kariuomenei';	
+		
+		} elseif($statusas == 'studijuoja') {
+			echo $vardas . ' netinkamas kariuomenei';
+		}
+	} elseif($amzius >= 27) {
+			echo $vardas . ' netinkamas kariuomenei';
+	}
+	
+
+} elseif($amzius >=65) {
+	echo $vardas . ' yra pensininkas';
+	echo $vardas . ' netinkamas kariuomenei';
+	
+} elseif($amzius < 0) {
+	echo 'Klaida. blogai nurodytas amzius';
+}
 
 ?>
