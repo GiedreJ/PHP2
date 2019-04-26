5f58ad6adc787fee285896e8873047bd23a7c684
Giedre <saulevakarine1@gmail.com>
2019-04-26 11:21:05 +0300

    function

--- index.php
+++ index.php
@@ -1,47 +1,18 @@
 <?php
 
-$mano_atmintis = [
-        'Penktadienis',
-        'Paskaita',
-        'Baras',
-        'Viskis',
-        'Alus',
-        'Degtine',
-        'Alus',
-        'Pirmadienis',
-        'Paskaita',
-];
+function amzius($amzius) {
+if ($amzius < 0) {
+    return 'klaida';
+    }
+if ($amzius < 18) {
+    return 'nepilnametis';
+    }
+if ($amzius < 65) {
+    return 'pilnametis';
+    }    
+    return 'pensininkas';
+}
 
-$random_flashback = rand(0, count($mano_atmintis) -1);
+print amzius();
 
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Užduotis5/6</title>
-        <link rel="stylesheet" href="/include/normalise.css">
-        <link rel="stylesheet" href="/include/grid.css">
-        <link rel="stylesheet" href="/include/style.css">
-    </head>
-    <body>
-        <h1>
-            Kas buvo pirmadienį?
-        </h1>
-        <h2>
-            Mano atmintis
-        </h2>
-        <h3>
-            <?php print '#' . $random_flashback . ':' . ' ' . $mano_atmintis[$random_flashback]; ?>
-        </h3>
-        <p>
-            <ul>
-                <?php foreach($mano_atmintis as $value): ?>
-                    <li> 
-			<?php print ($value); ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </p>
-        
-    </body>
-</html>
