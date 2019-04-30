bf308a79913db836db5a19d1ff8654061faaf702
Giedre <saulevakarine1@gmail.com>
2019-04-30 09:30:15 +0300

    rasinys

--- index.php
+++ index.php
@@ -1,33 +1,29 @@
 <?php
 
-$atrinkti_zodziai = [];
+$rasinys = [];
+$rasinio_ilgis = rand(100, 300);
 $zodziai = [
     'jonas', 'dviratis', 'laukas', 'bėga', 'krenta', 'alus',
 ];
 
-foreach ($zodziai as $zodis) {
-    $pateko = rand(0, 1);
-    
-    //var_dump("tikrinam zodi: $zodis, pateko: $pateko");    
-    
-    if ($pateko) {
-        $atrinkti_zodziai[] = $zodis;
-    }
+for ($x = 0; $x < $rasinio_ilgis; $x++) {
+    $random_index = rand(0, count($zodziai) -1);
+    $random_zodis = $zodziai[$random_index];
+    $rasinys[] = $random_zodis;
 }
+
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ataskaita</title>
+        <title>Rasinys</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text_balance/css" href="style.css">
     </head>
     <body>
-        <ul> 
-            <?php foreach ($atrinkti_zodziai as $atrinktas_zodis): ?>
-                <li><?php print $atrinktas_zodis; ?></li>
+            <?php foreach ($rasinys as $zodis): ?>
+                <span><?php print $zodis; ?></span>
             <?php endforeach; ?>
-        </ul>
     </body>
 </html>
