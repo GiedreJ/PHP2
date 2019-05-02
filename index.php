05d5ceff439cc16a1f5b0871c8e4fcc0300c69b0
Giedre <saulevakarine1@gmail.com>
2019-05-02 14:21:10 +0300

    5x5 kvadratukai

--- index.php
+++ index.php
@@ -1,26 +1,55 @@
 <?php
-/**
- * 
- * @return array
- */
-function tekstas($text) {
-    if (mb_strlen($text) <= 100) {
-        return $text;
-    }
-        return mb_strcut($text, 0, 97) . '...'; 
-}
 
-$text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."; 
-print tekstas($text) . '<br>';
-        
-?>     
+       
+function slot_run()
+{
+    $array = [
+        [rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1)],
+        [rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1), rand(0, 1)]
+    ];
+    return $array;
+}
+$array1 = slot_run();
+?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>3 X 3</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css">       
-    </head>
-    <body>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Kvadrateliai</title>
+    <style>
+        div {
+        border: 3px solid black;
+        padding: 10px;
+        margin: 10px;
+}
+section {
+        display: flex;
+        justify-content: center;
+        align-items: baseline;
+}
+.blue {
+        background-color: blue;
+}
+.orange {
+        background-color: orange;
+}
+    </style>
+</head>
+<body>
+<?php foreach ($array1 as $index): ?>
+    <section>
+        <?php foreach ($index as $value): ?>
+            <?php if ($value > 0): ?>
+                <div class="blue"></div>
+            <?php else: ?>
+                <div class="orange"></div>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </section>
+<?php endforeach; ?>
+</body>
 </html>
