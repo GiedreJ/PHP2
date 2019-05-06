e04a2f681e053b2b162624ccd24fd705d934f708
Giedre <saulevakarine1@gmail.com>
2019-05-06 09:57:05 +0300

    penktadienio nuostabi uzduotele...

--- index.php
+++ index.php
@@ -1,101 +1,127 @@
 <?php
+
+// randomizuoja pz2a lygį
+$randomise = rand(0,4);
+
+// Visų galimų istorijų sąrašas
+$istorijos = [
+    'nieko' => [
+        'prasidėjo diena'
+    ],
+    'px' => [
+        'nesuskambėjo žadintuvas',
+        'skauda galvą',
+        'nėra kavos',
+        'mano eilė vedžioti šunį'
+    ],
+    'bl' => [
+        'sudužo telefonas',
+        'nesikuria mašina',
+        'rytinis kamštis > 100km'
+    ],
+    'nx' => [
+        'neveikia php',
+        'nėra чумак',
+        'nėra grikių',
+    ],
+    'pz2a' => [
+        'pasirodo reikėjo \'empty cache\'',
+        'įpushinau į master branch'
+    ]
+];
+
 /**
- * Generuoja dinaminę matricą;
- * @param integr $size Matricos dydis;
- * @return array;
+ * 
+ * @param type $random_integer
+ * @param type $istorijos
+ * @return type array
  */
-function slot_run($size) {
-    $array = [];
-    for ($a = 0; $a < $size ; $a++) {
-        $row = [];
-        for ($b = 0; $b < $size; $b++) {
-            $row[] = rand(0, 1);
-        }
-        $array[] = $row;
-    } 
-    return $array;
-}
-
-function winning_slot($matrix){
-    $winning_rows = [];
+function listas($random_integer, $istorijos) {
+    $listas = [];
     
-    foreach ($matrix as $index => $row){
-        $row_winning_sum = count($row);
-        $column_sum = 0;
-        
-        foreach ($row as $column){
-            $column_sum += $column;    
-        } 
-        if ($row_winning_sum == $column_sum){
-            $winning_rows[] = $index; 
-        }
-        if ($column_sum == 0){
-            $winning_rows[] = $index; 
-        }
+    if ($random_integer == 0 || $random_integer > 0) {
+        $listas[0]['story'] = $istorijos['nieko'][0];
+        $listas[0]['text_color'] = 'text-green';
     }
-    
-    return $winning_rows;
-} 
-
-$size = rand(1, 10);
-$array = slot_run($size);
-
-$winning_rows = winning_slot($array);
-var_dump($winning_rows);
-
-$winning = count($winning_rows);
-if ($winning >0) {
-    $text = "Tau iškrito $winning. Tu - laimėjai!";
-} else {
-    $text = 'Tau akljdksjd!';
+    if ($random_integer > 0) {
+        $listas[1]['story'] = $istorijos['px'][rand(0, count($istorijos['px']) - 1)];
+        $listas[1]['text_color'] = 'text-green';
+        $listas[1]['css_color'] = 'green';
+        $listas[1]['meter_text'] = 'PX';
+    }
+    if ($random_integer > 1) {
+        $listas[2]['story'] = $istorijos['bl'][rand(0, count($istorijos['bl']) - 1)];
+        $listas[2]['text_color'] = 'text-yellow';
+        $listas[2]['css_color'] = 'yellow';
+        $listas[2]['meter_text'] = 'BL';
+    }
+    if ($random_integer > 2) {
+        $listas[3]['story'] = $istorijos['nx'][rand(0, count($istorijos['nx']) - 1)];
+        $listas[3]['text_color'] = 'text-orange';
+        $listas[3]['css_color'] = 'orange';
+        $listas[3]['meter_text'] = 'NX';
+    }
+    if ($random_integer > 3) {
+        $listas[4]['story'] = $istorijos['pz2a'][rand(0, count($istorijos['pz2a']) - 1)];
+        $listas[4]['text_color'] = 'text-red';
+        $listas[4]['css_color'] = 'red';
+        $listas[4]['meter_text'] = 'PZ2A';
+    }
+    return $listas;
 }
+
+/**
+ * Level meter
+ * @param type $feedme
+ * @return type integer or string
+ */
+function level($feedme) {
+    if ((count($feedme) - 1) < 4) {
+        return $level = count($feedme) - 1;
+    } else {
+        return $level = '!!MAX!!!';
+    }
+}
+
+$sugeneruotas_listas = listas($randomise, $istorijos);
+$level = level($sugeneruotas_listas);
 
 ?>
-
-<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="1">
-    <title>Kvadrateliai</title>
-    <style>
-        section {
-        display: flex;
-        justify-content: center;
-        align-items: baseline;
-}
-
-        div {
-        border: 3px solid black;
-        padding: 10px;
-        margin: 10px;
-}
-
-        section {
-        display: flex;
-        justify-content: center;
-        align-items: baseline;
-}
-        .blue {
-        background-color: blue;
-}
-        .orange {
-        background-color: orange;
-}
-    </style>
-</head>
-<body>
-    <p><?php print $text; ?></p>
-    <?php foreach ($array as $index): ?>
-    <section>
-        <?php foreach ($index as $value): ?>
-            <?php if ($value): ?>
-                <div class="blue"></div>
-            <?php else: ?>
-                <div class="orange"></div>
-            <?php endif; ?>
-        <?php endforeach; ?>
-     </section>
-    <?php endforeach; ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>PZ2A-MAT</title>
+        <link rel="stylesheet" type="text/css" href="include/normalise.css">
+        <link rel="stylesheet" type="text/css" href="include/style.css">
+    </head>
+    <body>
+        <div class="termometras">
+            <div class="apskritimas green">
+                <span><?php print $level; ?></span>
+            </div>
+            <div class="square <?php print $sugeneruotas_listas[1]['css_color']; ?>">
+                <span><?php print $sugeneruotas_listas[1]['meter_text']; ?></span>
+            </div>
+            <div class="square <?php print $sugeneruotas_listas[2]['css_color']; ?>">
+                <span><?php print $sugeneruotas_listas[2]['meter_text']; ?></span>
+            </div>
+            <div class="square <?php print $sugeneruotas_listas[3]['css_color']; ?>">
+                <span><?php print $sugeneruotas_listas[3]['meter_text']; ?></span>
+            </div>
+            <div class="square <?php print $sugeneruotas_listas[4]['css_color']; ?>">
+                <span><?php print $sugeneruotas_listas[4]['meter_text']; ?></span>
+            </div>
+        </div>
+        <div class="list">
+            <ul>
+                <?php foreach ($sugeneruotas_listas as $index): ?>
+                <li>
+                    <span class="<?php print $index['text_color']; ?>">
+                        <?php print $index['story']; ?>
+                    </span>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </body>
 </html>
