da328d04fa5ac939d5ec00fd1aa0dbb409b21eba
Giedre <saulevakarine1@gmail.com>
2019-05-06 15:02:35 +0300

    friday linksma uzduotele

--- index.php
+++ index.php
@@ -1,15 +1,61 @@
 <?php
 
-function Vynas_alus($gender, $age){
-    if($age >= 20) {
-        if ($gender == 'male') {
-            print 'Alus';
-        } else {
-            print 'Vynas';
-        }
-    }
-};
+$css_class = 'pzda-level-' . rand(1, 4);
 
-Vynas_alus('female', 20)
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Klasės darbas, 05.02</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <style>
+            .pzdamat div {
+                display: inline-block;
+            }
+            
+            .pzdamat div span {
+                display: none;
+            }
+            
+            .lygis-0 {
+                width: 50px;
+                height: 50px;
+                background: green;
+                border-radius: 50%;
+            }
+            
+            .staciakampis {
+                background: gray;
+                width: 100px;
+                height: 50px;
+            }
+            
+            .pzda-level-1 .lygis-1 {
+                background: green;
+            }
+            
+            .pzda-level-1 .lygis-1 span {
+                display: inline-block;
+            }
+            
+            .pzda-level-2 .lygis-1 {
+                background: green;
+            }             
+            
+            .pzda-level-2 .lygis-2 {
+                background: green;
+            }            
+        </style>        
+    </head>
+    <body>
+        <div class="pzdamat pzda-level-2">
+            <div class="lygis-0"></div>
+            <div class="lygis-1 staciakampis"><span>PX</span></div>
+            <div class="lygis-2 staciakampis"><span>BL</span></div>
+            <div class="lygis-3 staciakampis"><span>NX</span></div>
+            <div class="lygis-4 staciakampis"><span>PZDA</span></div>
+        </div>
+    </body>
+</html>
 
-?> 
