64ad69a62789908cffbfde63a055a1d965ee0e81
Giedre <saulevakarine1@gmail.com>
2019-05-07 09:35:33 +0300

    grazios/protingos

--- index.php
+++ index.php
@@ -1,61 +1,97 @@
 <?php
 
-$css_class = 'pzda-level-' . rand(1, 4);
+$panos = [
+        [
+        'vardas' => 'Monika',
+        'grazi' => true,
+        'protinga' => false,
+    ],
+        [
+        'vardas' => 'Greta',
+        'grazi' => true,
+        'protinga' => true,
+    ],
+        [
+        'vardas' => 'Egle',
+        'grazi' => false,
+        'protinga' => false,
+    ],
+        [
+        'vardas' => 'Agne',
+        'grazi' => true,
+        'protinga' => true,
+    ]
+];
+
+var_dump($panos);
+
+
+function vardai($names){
+    return $names[rand(0, count($names) - 1)]['vardas'];
+}
+/**
+function random_name($panos) {
+    $random_index = rand(0, count($panos) -1);
+    return $panos[$random_index]['vardas'];
+}
+var_dump($random_name($panos));
+*/
+
+function grazios_protingos($merginos) {
+    $grazios_protingos = [];
+    
+    foreach ($merginos as $mergina) {
+        if ($mergina['grazi'] && $mergina['protinga']) {
+            $grazios_protingos[] = $mergina;
+        }
+    }
+    
+    return $grazios_protingos;
+}
+
+function kiek_graziu($merginos) {
+    $kiek = 0;
+    foreach ($merginos as $mergina) {
+
+        if ($mergina['grazi']) {
+            $kiek++;
+        }
+    }
+    $kiek_is_viso = count($merginos);
+    return $kiek / $kiek_is_viso * 100;
+}
+
+$kiek = kiek_graziu($panos);
+var_dump($kiek);
+
+$zmonos = grazios_protingos($panos);
+var_dump($zmonos);
+$random_mergina = vardai($panos);
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Klasės darbas, 05.02</title>
+        <title>Klasės darbas, 05.06</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <style>
-            .pzdamat div {
-                display: inline-block;
-            }
-            
-            .pzdamat div span {
-                display: none;
-            }
-            
-            .lygis-0 {
-                width: 50px;
-                height: 50px;
-                background: green;
-                border-radius: 50%;
-            }
-            
-            .staciakampis {
-                background: gray;
-                width: 100px;
-                height: 50px;
-            }
-            
-            .pzda-level-1 .lygis-1 {
-                background: green;
-            }
-            
-            .pzda-level-1 .lygis-1 span {
-                display: inline-block;
-            }
-            
-            .pzda-level-2 .lygis-1 {
-                background: green;
-            }             
-            
-            .pzda-level-2 .lygis-2 {
-                background: green;
-            }            
         </style>        
     </head>
     <body>
-        <div class="pzdamat pzda-level-2">
-            <div class="lygis-0"></div>
-            <div class="lygis-1 staciakampis"><span>PX</span></div>
-            <div class="lygis-2 staciakampis"><span>BL</span></div>
-            <div class="lygis-3 staciakampis"><span>NX</span></div>
-            <div class="lygis-4 staciakampis"><span>PZDA</span></div>
-        </div>
+    <ul>
+        <?php foreach ($zmonos as $mergina): ?>
+        <li>
+            <?php print $mergina['vardas']; ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+    <p>
+    <?php print $random_mergina; ?>
+    </p>
+    <h2>
+        <?php print "Gražių panų $kiek %"; ?> 
+    </h2>
     </body>
 </html>
 
