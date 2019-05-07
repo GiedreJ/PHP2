729b4139ca9b69f3629f71b7d3f71120d23b7f36
Giedre <saulevakarine1@gmail.com>
2019-05-07 10:56:11 +0300

    produktu katalogas su procentine nuolaida

--- index.php
+++ index.php
@@ -1,97 +1,73 @@
 <?php
 
-$panos = [
-        [
-        'vardas' => 'Monika',
-        'grazi' => true,
-        'protinga' => false,
+$catalog = [
+    [   'name' => 'Jeans',
+        'price' => '30',
+        'description' => 'Blue',
+        'discount' => '10',
     ],
-        [
-        'vardas' => 'Greta',
-        'grazi' => true,
-        'protinga' => true,
+    [   'name' => 'Jacket',
+        'price' => '40',
+        'description' => 'Black',
+        'discount' => '20',     
     ],
-        [
-        'vardas' => 'Egle',
-        'grazi' => false,
-        'protinga' => false,
+    [   'name' => 'Shirt',
+        'price' => '30',
+        'description' => 'Green',
+        'discount' => '5',     
     ],
-        [
-        'vardas' => 'Agne',
-        'grazi' => true,
-        'protinga' => true,
-    ]
+    [   'name' => 'Dress',
+        'price' => '30',
+        'description' => 'Pink',
+        'discount' => '10',     
+    ],
 ];
 
-var_dump($panos);
-
-
-function vardai($names){
-    return $names[rand(0, count($names) - 1)]['vardas'];
-}
-/**
-function random_name($panos) {
-    $random_index = rand(0, count($panos) -1);
-    return $panos[$random_index]['vardas'];
-}
-var_dump($random_name($panos));
-*/
-
-function grazios_protingos($merginos) {
-    $grazios_protingos = [];
-    
-    foreach ($merginos as $mergina) {
-        if ($mergina['grazi'] && $mergina['protinga']) {
-            $grazios_protingos[] = $mergina;
-        }
-    }
-    
-    return $grazios_protingos;
-}
-
-function kiek_graziu($merginos) {
-    $kiek = 0;
-    foreach ($merginos as $mergina) {
-
-        if ($mergina['grazi']) {
-            $kiek++;
-        }
-    }
-    $kiek_is_viso = count($merginos);
-    return $kiek / $kiek_is_viso * 100;
-}
-
-$kiek = kiek_graziu($panos);
-var_dump($kiek);
-
-$zmonos = grazios_protingos($panos);
-var_dump($zmonos);
-$random_mergina = vardai($panos);
+foreach ($catalog as $product_index => $product_value) {
+        $catalog[$product_index]['price_with_discount'] = $catalog[$product_index]['price'] * (100 - $catalog[$product_index]['discount']) / 100; 
+    };
+var_dump($catalog);
 
 ?>
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>Klasės darbas, 05.06</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <style>
-        </style>        
-    </head>
-    <body>
-    <ul>
-        <?php foreach ($zmonos as $mergina): ?>
-        <li>
-            <?php print $mergina['vardas']; ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-    <p>
-    <?php print $random_mergina; ?>
-    </p>
-    <h2>
-        <?php print "Gražių panų $kiek %"; ?> 
-    </h2>
-    </body>
+<head>
+    <meta charset="UTF-8S">
+    <title>Hey Hey</title>
+    <style>
+        .produktas {
+            display: flex;
+            height: 50px;
+            text-align: center;
+            font-weight: bold;
+        }
+        .stulpelis {
+            border: 1px solid black;
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <?php foreach ($catalog as $product): ?>
+    <div class="produktas">
+       <span class="stulpelis">
+           <?php print $product['name']; ?>
+       </span>
+        <span class="stulpelis">
+            <?php print $product['price'] . ' Eur'; ?>
+        </span>
+        <span class="stulpelis">
+             <?php print $product['description']; ?>
+        </span>
+        <span class="stulpelis">
+            <?php print $product['discount'] . ' %'; ?>
+        </span>
+        <span class="stulpelis">
+            <?php print $product['price_with_discount'] . ' Eur'; ?>
+        </span>
+    </div>
+    <?php endforeach; ?>
+</body>
 </html>
-
