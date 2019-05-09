d2532875022493f1e1ffef8741d1855d87f88135
Giedre <saulevakarine1@gmail.com>
2019-05-09 09:51:15 +0300

    Formos mokomes6

--- index.php
+++ index.php
@@ -1,42 +1,30 @@
 <?php
 
-/**
- * Kintamojo kelimas kvadratu
- * @param integer $x
- * @return integer
- */
-function square($x) {
-    return $x ** 2;
+$result = 0;
+
+if (isset($_POST['done'])) {
+    $result = ++$_POST['done'];
 }
 
-// Sąlyga inputo langui.
-if (!empty($_POST)) {
-    if ($_POST['action'] == 'pakelti') {
-        $message = 'Atsakymas: ' . square($_POST['skaicius']);
-    } elseif(($_POST['action'] == 'saknis')) {
-        $message = 'Atsakymas: ' . sqrt($_POST['skaicius']);
-    }
-} else {
-    $message = 'Įvesk skaičių!';
+$images = [];
+
+for ($i = 0; $i < $result; $i++) {
+    $images[$i] = 'banana.jpg';
 }
 
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Form Basics</title>
-    </head>
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <title>Bananas</title> 
+        <meta charset="utf-8"> 
+    </head> 
     <body>
-        <form method="POST">
-            <label>
-                <span> Ką pakelti kvadratu:</span>
-                <input type="number" name="skaicius">
-            </label>
-            
-            <button name="action" value="pakelti">Bandom kvadarata!</button>      
-            <button name="action" value="saknis">Bandom sakni!</button>      
-            <input type="reset">
+        <form method="post">
+            <input type="submit" name="done" value="<?php print $result ?>">
         </form>
-        <p><?php print $message; ?></p>
-    </body>
-</html>
+        <?php foreach ($images as $i): ?>
+            <img src="<?php print $i; ?>">
+        <?php endforeach; ?>
+    </body> 
+</html> 
